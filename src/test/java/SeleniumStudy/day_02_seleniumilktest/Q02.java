0bff6ece72ac1b2bc3dbfe602dcfa7d6665a0779
sagun33 <salihguenay33@gmail.com>
2022-12-26 18:07:07 +0100

    m

---
+++ src/test/java/SeleniumStudy/day_02_seleniumilktest/Q02.java
@@ -0,0 +1,11 @@
+package SeleniumStudy.day_02_seleniumilktest;
+
+public class Q02 {
+    ////1-Test01 isimli bir class olusturun
+    //    //2- https://www.amazon.com/ adresine gidin
+    //    //3- Browseri tam sayfa yapin
+    //    //4-Sayfayi “refresh” yapin
+    //    //5-”Salesforce Apex Questions Bank” icin arama yapiniz
+    //    //6- Kac sonuc bulundugunu yaziniz
+    //    //7-Sayfayi kapatin
+}
