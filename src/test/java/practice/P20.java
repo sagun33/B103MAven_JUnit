bcd230a7eb13c37ac3de6d5a9818669867b24c7d
sagun33 <salihguenay33@gmail.com>
2023-01-17 20:46:08 +0100

    Merge remote-tracking branch 'origin/master'

---
+++ src/test/java/practice/P20.java
@@ -0,0 +1,23 @@
+package practice;
+
+import org.junit.Test;
+
+public class P20 {
+    @Test
+    public void name() {
+
+        //                               - FAKER -
+        //        Faker class'ı, adlar, telefon numaraları, adresler, SSN vb.
+        //    gibi gerçek görünümlü verileri dinamik olarak oluşturmanıza olanak tanır.
+        //    Web uygulamanızın bunları doğru bir şekilde kaydedeceğini bildiğiniz sürece,
+        //    çoğu zaman ad veya adres gibi bu değerlerin ne olduğu umurunuzda olmaz ve faker kullanmak
+        //    bu verileri modellemenize ve bu alanlar için şimdiye kadar test çalıştırması için yeni dinamik
+        //    değerler oluşturmanıza olanak tanır.
+        //        Faker class'ını kullanabilmemiz için mvn repository adresinden java faker kütüphanesini
+        //    projemize eklememiz gerekmektedir.
+        //
+        //    //"https://demoqa.com/automation-practice-form"  Adresine gidin
+        //    // Formu doldurunuz
+        //    // Sayfayi kapatin
+    }
+}
