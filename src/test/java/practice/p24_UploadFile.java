bce1a13fd927c00e5d9a52b63edf5c1dd7cbff1b
sagun33 <salihguenay33@gmail.com>
2023-01-19 00:50:07 +0100

    Merge remote-tracking branch 'github/master'

---
+++ src/test/java/practice/p24_UploadFile.java
@@ -0,0 +1,39 @@
+package practice;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import utilities.TestBase;
+
+public class p24_UploadFile extends TestBase {
+    //https://demo.guru99.com/test/upload/ adresine gidiniz
+    //Bilgisayarınızdan herhangi bir dosyayı upload ediniz
+    // I accept terms of service bölümünü tikleyin
+    // Submit File buttonuna basınız
+    // "1 file has been successfully uploaded" görünür olduğunu test edin
+
+    @Test
+    public void name() {
+        //https://demo.guru99.com/test/upload/ adresine gidiniz
+        driver.get("https://demo.guru99.com/test/upload/");
+
+        //Bilgisayarınızdan herhangi bir dosyayı upload ediniz
+        //"C:\Users\Lenovo\OneDrive\Masaüstü\sen.txt" //DOSYANIN GERÇEK YOLUNU ALIP GÖREBİLMEK BURAYA KOYDUK
+
+        //String farkliKisim = System.getProperty("user.home");//C:\Users\Lenovo
+        //String ortakYol = "\\OneDrive\\Masaüstü\\sen.txt";
+        //String dosyaYolu = farkliKisim+ortakYol;
+
+        String dosyaYolu = "C:\\Users\\Lenovo\\OneDrive\\Masaüstü\\sen.txt";
+
+        WebElement dosyaSec = driver.findElement(By.xpath("//*[@id='uploadfile_0']"));
+        dosyaSec.sendKeys(dosyaYolu);
+        // I accept terms of service bölümünü tikleyin
+        WebElement checkBox =  driver.findElement(By.xpath("//*[@id='terms']"));
+        checkBox.click();
+        checkBox.sendKeys(Keys.TAB,Keys.TAB,Keys.ENTER);
+        // Submit File buttonuna basınız
+        // "1 file has been successfully uploaded" görünür olduğunu test edin
+    }
+}
