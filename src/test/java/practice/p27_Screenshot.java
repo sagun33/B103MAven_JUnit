82ea12d1bd5f0f68f9ca34ad308c0d892f007919
sagun33 <salihguenay33@gmail.com>
2023-01-18 21:47:35 +0100

    Merge remote-tracking branch 'github/master'

---
+++ src/test/java/practice/p27_Screenshot.java
@@ -0,0 +1,17 @@
+package practice;
+
+import org.junit.Test;
+import utilities.TestBase;
+
+public class p27_Screenshot extends TestBase {
+    @Test
+    public void name() {
+        //Amazon sayfasına gidelim
+        driver.get("https://www.amazon.com");
+        //Tüm sayfanın resmini alalım
+        //Yeni bir sekmede techproeducation sayfasına gidip sayfanın resmini alalım
+        //Tekrar amazon sayfasına dönüp iphone aratalım
+        //Arama sonucunun resmini alalım
+
+    }
+}
